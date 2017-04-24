3b529e729af69274d26effa526e13308b88578e3
geobi <geobi>
2017-04-24 16:33:24 +0100

    First commit

---
+++ WebCRM/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCRM.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        public ActionResult Customer()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+	}
+}
